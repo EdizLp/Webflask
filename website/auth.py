10520bfd670e8554853be91dd401e272d52f9dc7
ediz333lp <edizsuleyman404@gmail.com>
2026-02-06 21:51:57 +0000

    Added more stuff to Templates

--- website/auth.py
+++ website/auth.py
@@ -1,16 +1,19 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 #this file is for authentication
 
 auth = Blueprint('auth', __name__)#this defines our blueprint, basically saying a different sector of my code
 
+
+
 @auth.route('/login')
 def login():
-    return "<p>Login</p>"
+    return render_template("login.html", text = "testing", user = "Ediz") #this is our html, well in this case our template, you can even pass variables with Jinja 
 
 @auth.route('/logout')
 def logout():
-    return "<p>logout</p>"#this is our html
+    return "<p>Logout</p>"  
+
 @auth.route('/sign-up')
 def sign_up():
-    return "<p>Sign Up</p>"
+    return render_template("sign_up.html")
 
