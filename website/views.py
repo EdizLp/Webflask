8d027ec6348cfaa34f71739a8f92d83627bd1e09
Ediz Suleyman <edizsuleyman404@gmail.com>
2026-02-07 19:46:59 +0000

    Update views.py

--- website/views.py
+++ website/views.py
@@ -1,9 +1,9 @@
-#this is for .... 
-from flask import Blueprint
+#this is for our blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)#this defines our blueprint
 
 @views.route('/')#a route is the specific url and we use @ to define those. Basically when they go to a certain page run this code
 # we used / as this is just the home address
 def home():
-    return "<h1>Test</h1>"
+    return render_template("home.html")
